502d98f1c68a56081f8f5d1628c51d04f075425c
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2023-05-01 09:38:01 +0300

    chamges6

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,8 +5,8 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:reset RAILS_ENV=production
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate RAILS_ENV=production
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:reset RAILS_ENV=production
+bundle exec rake db:migrate 
 # # bundle exec rake db:drop
 # # bundle exec rake db:create
-# bundle exec rake db:seed
+bundle exec rake db:seed
